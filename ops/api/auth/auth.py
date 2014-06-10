9baeb1d5714c2c0b4143e2eb6e82abffdaf9951e
coocla <327052034@qq.com>
2014-06-10 05:24:54 +0000

    modified redis storage keystone object

--- ops/api/auth/auth.py
+++ ops/api/auth/auth.py
@@ -1,6 +1,7 @@
 #-*- coding:utf-8 -*-
 import tornado.web
 import redis
+import cPickle
 
 from keystoneclient.v2_0 import client
 from ops.api.auth import policy
@@ -15,42 +16,45 @@ class BaseAuth(tornado.web.RequestHandler):
 
     def _auth(self, request):
         token = request.headers.get("X-Auth-Token", None)
-        print "============================"
-        print token
         if not token:
             """Reject the request"""
-            #raise tornado.web.HTTPError(404)
-            print "xxxxxx111"
-            return self.write("deny!!!!")
-            print "xxxxxx222"
+            self.set_status(404)
+            self._transforms = []
+            return self.finish("<html><body><h1>404 Not Found</h1></body></html>")
         self._auth_by_token(token)
 
     def _auth_by_token(self, token):
         """
-        Auth
+        By token Authenticate roles
         """
         backend = Backend()
         user_has_roles = backend.get_user_roles(token)
-        print "Redis:",user_has_roles
         if not user_has_roles:
             user_has_roles = self.get_roles_from_keystone(token)
-            backend.set_user_roles(token, user_has_roles)
-        print "Last:",user_has_roles
-        print "Method:", self.method_mapping_roles()
+            backend.set_user_msg(token, self.get_usermsg_from_keystone)
         if not filter(lambda x: x in user_has_roles, self.method_mapping_roles()):
             """Reject the request"""
-            print "deny~~~~~~~"
+            self.set_status(401)
+            self._transforms = []
+            return self.finish("<html><body><center><h1>401 Authorization Required</h1></center></body></html>")
+
+    def get_usermsg_from_keystone(self, token):
+        """
+        Return object from keystone by token 
+        """
+        try:
+            return client.Client(token=token, endpoint=self.endpoint)
+        except:
+            return None
 
     def get_roles_from_keystone(self, token):
         """
-        Return a list about roles by token
-        ["admin", "common"]
+        Return list with roles by token
         """
-        try:
-            keystone = client.Client(token=token, endpoint=self.endpoint)
-            return [role.name for role in keystone.roles.list()]
-        except:
-            return []
+        usermsg = self.get_usermsg_from_keystone(token)
+        if usermsg:
+            return map(lambda role: role.name, usermsg.roles.list())
+        return []
 
     def method_mapping_roles(self):
         """
@@ -68,27 +72,35 @@ class Backend(object):
     def __init__(self):
         self.conn = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-    def get_user_roles(self, token):
+    def get_user_msg(self, token):
         """
-        Return list about token from redis-server
+        Return object with token
         """
         try:
-            ret = self.conn.lrange(token, 0, -1)
+            ret = self.conn.get(token)
+            if ret:
+                ret = cPickle.loads(ret)["msg"]
         except:
-            ret = []
+            ret = None
         return ret
 
-    def set_user_roles(self, token, roles):
+    def set_user_msg(self, token, user_msg):
         """
-        Set user's token with roles into redis-server.
+        Set user's token with user msg into redis-server.
         Expire 900sec or 15min
         """
         try:
-            if isinstance(roles, (list, tunple)):
-                for role in roles:
-                    self.conn.lpush(token, role)
+            if user_msg:
+                msg = cPickle.dumps({"msg": user_msg})
+                self.conn.set(token, msg)
                 self.conn.expire(token, 900)
                 return True
         except:
             self.conn.delete(token)
             return False
+
+    def get_user_roles(self, token):
+        user_msg = self.get_user_msg(token)
+        if user_msg:
+            return map(lambda role: role.name, user_msg.roles.list())
+        return []
